36eec044c69b7b1f33f082b29b6f8449923b3132
ResponsiveAG <info@responsive.ag>
2015-10-26 14:21:12 +0100

    First
    First

---
+++ Andi/Assets/WebCam.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+        string backCamName = "";
+        for (int i = 0; i < devices.Length; i++)
+        {
+            Debug.Log("Device:" + devices[i].name + "IS FRONT FACING:" + devices[i].isFrontFacing);
+
+            if (!devices[i].isFrontFacing)
+            {
+                backCamName = devices[i].name;
+            }
+        }
+
+        WebCamTexture wct = new WebCamTexture(backCamName);
+
+        this.GetComponent<Renderer>().material.mainTexture = wct;
+        wct.Play();
+
+	}
+}
