e975b559b653cb997e5780c151ee25ef1a29cc2c
ResponsiveAG <info@responsive.ag>
2015-10-29 15:16:57 +0100

    Newest
    Blub

---
+++ Andi/Assets/WebCam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebCam : MonoBehaviour {
+
+
+    Renderer r;
+
+	// Use this for initialization
+	void Start () {
+
+        WebCamTexture wt = new WebCamTexture();
+        r = this.GetComponent<Renderer>();
+
+        r.material.mainTexture = wt;
+        wt.Play();
+	}
+}
